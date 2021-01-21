fd4bcaecd96d18f562bbf7ce775fe4cac34668e7
Maxim <sexmilitary@icloud.com>
2021-01-21 16:35:56 +0300

    New3

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,14 +3,14 @@ import Vapor
 
 func routes(_ app: Application) throws {
     
-    app.post("v1", "payment") { (req) -> EventLoopFuture<Payment> in
+    app.post("v", "payment") { (req) -> EventLoopFuture<Payment> in
         
         let payment = try req.content.decode(Payment.self)
         print(payment)
         return payment.create(on: req.db).map { payment }
     }
     
-    app.get("v1", "payments") { (req) -> EventLoopFuture<[Payment]> in
+    app.get("v", "payments") { (req) -> EventLoopFuture<[Payment]> in
         Payment.query(on: req.db).all()
     }
     
